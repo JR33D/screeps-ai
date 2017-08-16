ed9f28c155494257d363a399e576da779dde2ae9
Jeremy Reed <jreed129@gmail.com>
2017-08-16 16:55:52 -0400

    Add tsconfig, compile gulp task, and other items needed for typescript project.
    * Moved existing js code into js folder so it is still available while conversion happens.
    * Add Typings to have screep types available for development.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp');
 var screeps = require('gulp-screeps');
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
 
 var secrets = require('./secrets.js');
 
+var srcDist = 'dist/';
+
 gulp.task('upload', function () {
-    gulp.src('src/*.js')
+    gulp.src(srcDist + '*.js')
         .pipe(screeps(secrets));
 });
 
 gulp.task('ptr', function () {
     secrets.ptr = true;
-    gulp.src('src/*.js')
+    gulp.src(srcDist + '*.js')
         .pipe(screeps(secrets));
+});
+
+gulp.task("compile", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('./dist'));
 });
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+/**
+ * @export loop function as main loop for game.
+ *
+ */
+module.exports.loop = function () {
+
+};
